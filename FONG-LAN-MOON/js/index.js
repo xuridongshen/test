8aa1f333d65166f7249fa669edef4e3e5b6c285d
袁晨阳 <15861942487@163.com>
2018-11-23 13:22:33 +0800

    1

--- FONG-LAN-MOON/js/index.js
+++ FONG-LAN-MOON/js/index.js
@@ -1,6 +1,14 @@
+ var f_item_4 = $('.f-item-4')
+ var f_item_6 = $('.f-item-6')
  var f_item_9 = $('.f-item-9')
 
 
  setTimeout(function () {
-     f_item_9.css('background-image', 'url(../FONG-LAN-MOON/image/contactus.png)')
+    f_item_4.css('background-image', 'url(../FONG-LAN-MOON/image/img_storegront_a.png)')
+ }, 500)
+ setTimeout(function () {
+    f_item_6.css('background-image', 'url(../FONG-LAN-MOON/image/joining.png)')
  }, 1000)
+ setTimeout(function () {
+     f_item_9.css('background-image', 'url(../FONG-LAN-MOON/image/contactus.png)')
+ }, 2000)
